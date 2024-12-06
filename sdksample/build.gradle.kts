b8905b68fd2fd39c4e33eefac9fc287905bae8b6
koichi-ozaki <koichi.ozaki@gmo-media.jp>
2024-12-06 18:20:37 +0900

    isMinifyEnabled も消してみる

--- sdksample/build.gradle.kts
+++ sdksample/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+//            isMinifyEnabled = true
 //            proguardFiles(
 //                getDefaultProguardFile("proguard-android-optimize.txt"),
 //                "proguard-rules.pro"
